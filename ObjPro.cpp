2f6b50cb0ea1594d8d2825ed1de3a9c45d051db8
Patryk430 <patryk.survilo@mif.stud.vu.lt>
2025-02-18 20:24:03 +0200

    rankinis ir pazymiu generavimas

--- ObjPro.cpp
+++ ObjPro.cpp
@@ -3,12 +3,15 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <ctime>
+#include <random>
 
 int stud_skc;
 int temp;
 int paz_skc;
 bool rezultato_tipas; 
 int pasirinkimas_pradzia;
+
 
 
 struct studentas
@@ -24,54 +27,19 @@ struct studentas
 
 std::vector <studentas> stud;
 
+void pradzia();
+void rankinis();
+void automatinis();
+void rezultatas();
+void pazymiu_generacija(int i);
 
 int main()
 {
-	std::cout << "Kiek studentu?" << std::endl;
-	std::cin >> stud_skc;
+	srand (time(NULL));
 
-	for (int i = 0; i< stud_skc; i++)
-	{
-		std::cout << "vardas?" << std::endl;
-		std::cin >> stud.begin().vardas;
+ 	pradzia();
 
-		std::cout << "pavarde?" << std::endl;
-		std::cin >> stud[i].pavarde;
-
-		std::cout << "kiek pazymiu turi?" << std::endl;
-		std::cin >> paz_skc;
-
-		for (int j = 0; j < paz_skc; j++)
-		{
-			std::cout << "ivesk pazymi" << std::endl;
-			std::cin >> temp;
-			stud[i].pazymiai.push_back(temp);
-		}
-
-		std::cout << "kiek uz egzamina?" << std::endl;
-		std::cin >> stud[i].egzaminas;
-
-		int sum = std::accumulate(stud[i].pazymiai.begin(), stud[i].pazymiai.end(), 0);
-
-		stud[i].vidurkis = sum / stud[i].pazymiai.size();
-		stud[i].vidurkis = stud[i].vidurkis * 0.4 + stud[i].egzaminas * 0.6;
-
-		sort(stud[i].pazymiai.begin(), stud[i].pazymiai.end());
-
-		if (stud[i].pazymiai.size() % 2 != 0)
-			stud[i].mediana = (double)stud[i].pazymiai[stud[i].pazymiai.size() / 2];
-		else
-			stud[i].mediana = (double)(stud[i].pazymiai[(stud[i].pazymiai.size() - 1) / 2] + stud[i].pazymiai[stud[i].pazymiai.size() / 2]) / 2.0;
-	}
-
-	std::cout << "0-isvest vidurki 1-mediana" << std::endl;
-	std::cin >> rezultato_tipas;
-
-	for (int i = 0; i< stud_skc; i++)
-	{
-		if (rezultato_tipas) std::cout << stud[i].vardas << " " << stud[i].pavarde <<  " " << stud[i].mediana << std::endl;
-		else std::cout << stud[i].vardas << " " << stud[i].pavarde << " " << stud[i].vidurkis << std::endl;
-	}
+ 	return 0;
 }
 
 void pradzia()
@@ -89,13 +57,14 @@ void pradzia()
 	if (pasirinkimas_pradzia==3) rankinis();
 	if (pasirinkimas_pradzia==2) rankinis();
 	if (pasirinkimas_pradzia==1) automatinis();
+	
 
 };		
 
 void rankinis()
 {
 	bool studentu_pildymas=true;
-	i=0;
+	int i=0;
 
 	while (studentu_pildymas)
 	{
@@ -115,7 +84,7 @@ void rankinis()
 			bool pazymiu_pildymas=true;
 			while (pazymiu_pildymas)
 			{
-				std::cout << "įveskite" << i+1 << "-ojo  studento pažymį" << std::endl;
+				std::cout << "įveskite " << i+1 << "-ojo  studento pažymį" << std::endl;
 				std::cin >> temp;
 				stud[i].pazymiai.push_back(temp);
 
@@ -124,10 +93,54 @@ void rankinis()
 			}
 		}
 
+		if (pasirinkimas_pradzia==2) pazymiu_generacija(i);
+
 		i++;
 		std::cout<<"Ar dar yra studentų? jei ne įveskite '0' jei taip įveskite '1'" << std::endl;
 		std::cin>>studentu_pildymas;
 	};
 
+	rezultatas();
+};
+
+void rezultatas()
+{
+	std::cout << "0-isvest vidurki 1-mediana" << std::endl;
+	std::cin >> rezultato_tipas;
+
+	for (int i = 0; i< stud.size(); i++)
+	{
+		int sum = std::accumulate(stud[i].pazymiai.begin(), stud[i].pazymiai.end(), 0);
+
+		stud[i].vidurkis = sum / stud[i].pazymiai.size();
+		stud[i].vidurkis = stud[i].vidurkis * 0.4 + stud[i].egzaminas * 0.6;
 	
+		sort(stud[i].pazymiai.begin(), stud[i].pazymiai.end());
+	
+		if (stud[i].pazymiai.size() % 2 != 0)
+			stud[i].mediana = (double)stud[i].pazymiai[stud[i].pazymiai.size() / 2];
+		else
+			stud[i].mediana = (double)(stud[i].pazymiai[(stud[i].pazymiai.size() - 1) / 2] + stud[i].pazymiai[stud[i].pazymiai.size() / 2]) / 2.0;
+
+
+		if (rezultato_tipas) std::cout << stud[i].vardas << " " << stud[i].pavarde <<  " " << stud[i].mediana << std::endl;
+		else std::cout << stud[i].vardas << " " << stud[i].pavarde << " " << stud[i].vidurkis << std::endl;
+	}
+};
+
+void automatinis()
+{
+
+};
+
+void pazymiu_generacija(int i)
+{
+	std::cout<<"Kiek pažymių turi "<<i+1<<"-tasis studentas?"<<std::endl;
+	std::cin>>paz_skc;
+
+	for (int j=0; j<paz_skc; j++)
+	{
+	temp = 1 + rand() % (10-1)+1;
+	stud[i].pazymiai.push_back(temp);
+	};
 };
